98555e4986b5af1a2c3e09b23b9e5ea6e5acf867
Hosein <hosein720000@gmail.com>
2026-07-22 17:16:41 +0330

    Update contactService with production MockAPI URL

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "react-confirm-alert/src/react-confirm-alert.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/contact-manager-app">
         <App />
     </BrowserRouter>
   </React.StrictMode>
